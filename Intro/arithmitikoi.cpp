dd933c26a4d3c23f7ef7be2fabeb1fd7676658f4
Andreas Bourakis <bourakis@gmail.com>
2020-02-25 12:40:11 +0200

    Create arithmitikoi.cpp

---
+++ Intro/arithmitikoi.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(int argc, char *argv[])
+{
+    
+    int x,y;
+
+    cout << "Dwse prwto ari8mo: ";
+    cin >> x;
+    cout << "Dwse deytero ari8mo: ";
+    cin >> y;
+    
+    cout << endl;
+    
+    //Αριθμητικοί τελεστές
+    cout << "x+y: " << x+y << endl;
+    cout << "x-y: " << x-y << endl;
+    cout << "x*y: " << x*y << endl;
+    cout << "x/y: " << x/y << endl;
+    cout << "x%y: " << x%y << endl;
+    cout << "++x: " << ++x << endl;
+    cout << "--x: " << --x << endl << endl;
+        
+    system("PAUSE");
+    
+    return 0;
+}
